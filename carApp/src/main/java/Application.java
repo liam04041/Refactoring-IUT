5a63da576e11d0edf5d71581ebdcb3459cc3de49
liam04041 <liam.lucas414@gmail.com>
2026-03-13 14:51:46 +0100

    Ajout des tests exploitants le contenu de system.out + peaufinage de la tarduction du code. 14 tests OK.

--- carApp/src/main/java/Application.java
+++ carApp/src/main/java/Application.java
@@ -12,3 +12,4 @@ public class Application {
         System.out.println("fini");
     }
 }
+
--- carApp/src/test/java/DriverTest.java
+++ carApp/src/test/java/DriverTest.java
@@ -4,15 +4,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DriverTest {
 
-    private Driver driver;
+    @Test
+    public void Driver_should_be_adult_if_above_minimum_age_required() {
+        //GIVEN
+        Driver driver = new Driver("John", 10);
 
-    @BeforeEach
-    public void setUp() {
-        driver = new Driver("John", 20);
+        //THEN
+        assertThat(driver.isAdult()).isTrue();
     }
 
     @Test
-    public void testIsAdult() {
-        assertThat(driver.isAdult()).isTrue();
+    public void Driver_should_not_be_adult_below_minimum_age_required() {
+        //GIVEN
+        Driver driver = new Driver("John", 8);
+
+        //THEN
+        assertThat(driver.isAdult()).isFalse();
+    }
+
+    @Test
+    public void Getter_should_return_age_of_current_driver() {
+        //GIVEN
+        Driver driver = new Driver("John", 10);
+
+        //THEN
+        assertThat(driver.getAge()).isEqualTo(10);
     }
 }
